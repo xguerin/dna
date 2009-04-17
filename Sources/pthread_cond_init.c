f32c550e2b2d1211f46784342f905d3358e40ca8
Xavier Guérin <xavier.guerin@imag.fr>
2009-04-17 14:55:00 +0200

    Modified the semaphore_create signature to be more compliant with the others.

--- Sources/pthread_cond_init.c
+++ Sources/pthread_cond_init.c
@@ -4,17 +4,24 @@ int pthread_cond_init(pthread_cond_t *condition, pthread_condattr_t *condattr)
 {
 	static int32_t index = 0;
 	char name[32];
+  status_t status = DNA_OK;
 
 	sprintf (name, "pthread_cond_%ld", index ++);
   condition -> count = 0;
-	condition -> semaphore = semaphore_create (name, 0);
+	status = semaphore_create (name, 0, & condition -> semaphore);
 
-	switch (condition -> semaphore) {
-		case DNA_NO_MORE_SEM :
-			return EAGAIN;
-		case DNA_OUT_OF_MEM :
-			return ENOMEM;
-		default :
-			return 0;
-	}
+  if (status != DNA_OK)
+  {
+    switch (status)
+    {
+      case DNA_NO_MORE_SEM :
+        return EAGAIN;
+      case DNA_OUT_OF_MEM :
+        return ENOMEM;
+      default :
+        return EINVAL;
+    }
+  }
+
+  return 0;
 }
--- Sources/pthread_mutex_init.c
+++ Sources/pthread_mutex_init.c
@@ -4,16 +4,23 @@ int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr)
 {
 	static int32_t index = 0;
 	char name[32];
+  status_t status = DNA_OK;
 
 	sprintf (name, "pthread_mutex_%ld", index ++);
-	mutex -> semaphore = semaphore_create (name, 1);
+	status = semaphore_create (name, 1, & mutex -> semaphore);
 
-	switch (mutex -> semaphore) {
-		case DNA_NO_MORE_SEM :
-			return EAGAIN;
-		case DNA_OUT_OF_MEM :
-			return ENOMEM;
-		default :
-			return 0;
-	}
+  if (status != DNA_OK)
+  {
+    switch (status)
+    {
+      case DNA_NO_MORE_SEM :
+        return EAGAIN;
+      case DNA_OUT_OF_MEM :
+        return ENOMEM;
+      default :
+        return EINVAL;
+    }
+  }
+
+  return 0;
 }
