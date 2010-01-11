69b90d3f17971625ae7f7ee59bbc9fcad7b81514
Xavier Guérin <xavier.guerin@imag.fr>
2010-01-11 15:39:12 +0100

    Added updates.

--- Headers/Public/Processor/Cache.h
+++ Headers/Public/Processor/Cache.h
@@ -1,7 +1,22 @@
 #ifndef PROCESSOR_CACHE_H
 #define PROCESSOR_CACHE_H
 
-#define cpu_cache_wbflush()                   \
+#include <stdint.h>
+
+typedef enum cpu_cache
+{
+  CPU_CACHE_INSTRUCTION,
+  CPU_CACHE_DATA
+}
+cpu_cache_t;
+
+#define CPU_ICACHE_SIZE_LOG2 3
+#define CPU_DCACHE_SIZE_LOG2 3
+
+#define CPU_CACHE_ALL 0xFFFFFFFF
+
+
+#define cpu_cache_sync()                      \
 {                                             \
   volatile register int32_t dummy = 0;        \
                                               \
@@ -11,6 +26,9 @@
       : "r"(dummy)                            \
       );                                      \
 }
+
+extern void cpu_cache_invalidate (cpu_cache_t cache_type,
+    void * address, int32_t words);
 
 #endif
 
