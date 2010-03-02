042a08e51ef60ecac6550bb08fd48af8d36b9964
Xavier Guérin <xavier.guerin@imag.fr>
2010-03-02 09:53:49 +0100

    Added PLATFORM_ENDIANNESS. Added platform_endianness_t in both header file and component.xml.

--- Headers/Public/Platform/Endian.h
+++ Headers/Public/Platform/Endian.h
@@ -1,6 +1,13 @@
-#ifndef ENDIAN_H
-#define ENDIAN_H
+#ifndef PLATFORM_ENDIAN_H
+#define PLATFORM_ENDIAN_H
 
-#define PLATFORM_IS_LITTLE_ENDIAN
+#define PLATFORM_ENDIANNESS PLATFORM_LITTLE_ENDIAN
+
+typedef enum _platform_endianness
+{
+  PLATFORM_LITTLE_ENDIAN,
+  PLATFORM_BIG_ENDIAN
+}
+platform_endianness_t;
 
 #endif
