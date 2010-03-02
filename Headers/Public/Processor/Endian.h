f45162e62d2f8be8c7bf7683549f3f58a37e25a4
Xavier Guérin <xavier.guerin@imag.fr>
2010-03-02 09:52:46 +0100

    Added CPU_ENDIANNESS. Added cpu_endianness_t in both header file and component.xml.

--- Headers/Public/Processor/Endian.h
+++ Headers/Public/Processor/Endian.h
@@ -3,7 +3,16 @@
 
 #include <Platform/Platform.h>
 
-#define SWITCH_ENDIAN_32(x)                                     \
+#define CPU_ENDIANNESS CPU_LITTLE_ENDIAN
+
+typedef enum _cpu_endianness
+{
+  CPU_LITTLE_ENDIAN,
+  CPU_BIG_ENDIAN
+}
+cpu_endianness_t;
+
+#define SWITCH_ENDIAN_32(x)                                       \
   x =  ((x & 0x000000ff) << 24) | ((x & 0x0000ff00) << 8)  |      \
   ((x & 0x00ff0000) >> 8)  | ((x & 0xff000000) >> 24)       
 
@@ -12,22 +21,28 @@
 #define cpu_data_is_bigendian(type,data) SWITCH_ENDIAN_##type(data)
 #define cpu_data_is_littleendian(type,data)
 
-#if defined(PLATFORM_IS_BIG_ENDIAN)
+#ifndef PLATFORM_ENDIANNESS
+  #error "PLATFORM_ENDIANNESS not defined."
+#endif
+
+#if PLATFORM_ENDIANNESS == PLATFORM_BIG_ENDIAN
   #define cpu_platform_to_cpu(type,value) SWITCH_ENDIAN_##type(data)
   #define cpu_cpu_to_platform(type,value) SWITCH_ENDIAN_##type(data)
-#elif defined(PLATFORM_IS_LITTLE_ENDIAN)
+#elif PLATFORM_ENDIANNESS == PLATFORM_LITTLE_ENDIAN
   #define cpu_platform_to_cpu(type,value)
   #define cpu_cpu_to_platform(type,value)
 #else
-  #error "ERROR : Platform endianness not set."
+  #error "ERROR : Platform endianness not recognized."
 #endif
 
-#define cpu_data_concat(size,result,lvalue,hvalue) {    \
-  result = (hvalue << size) | lvalue;                    \
+#define cpu_data_concat(size,result,lvalue,hvalue)      \
+{                                                       \
+  result = (hvalue << size) | lvalue;                   \
 }
 
-#define cpu_data_split(size,value,lvalue,hvalue) {       \
-  hvalue = value >> size;                                \
+#define cpu_data_split(size,value,lvalue,hvalue)        \
+{                                                       \
+  hvalue = value >> size;                               \
   lvalue = value - (hvalue << size);                    \
 }
 
