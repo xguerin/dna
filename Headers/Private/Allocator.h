5abde18783549c377660acfc244af4accb880291
Xavier Guérin <xavier.guerin@imag.fr>
2010-04-12 13:10:25 +0200

    Some code refactor.

--- Headers/Private/Allocator.h
+++ Headers/Private/Allocator.h
@@ -98,13 +98,11 @@ extern uint32_t OS_KERNEL_HEAP_SIZE;
 
 extern kernel_allocator_t kernel_allocator;
 
-
 extern void * kernel_malloc (uint32_t size, bool erase);
 extern status_t kernel_free (void * area);
 
 extern status_t kernel_region_create (uint32_t required_nblocks,
     kernel_region_t * p_region);
-
 extern status_t kernel_region_destroy (kernel_region_t region);
 
 #endif
