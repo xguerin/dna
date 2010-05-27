29f492dca40a4e436e750f252291249b04466ca1
Xavier Guérin <xavier.guerin@imag.fr>
2010-05-27 23:31:35 +0200

    Fixed some typos.

--- Headers/Public/DnaTools/C.h
+++ Headers/Public/DnaTools/C.h
@@ -22,10 +22,10 @@
 #include <stdint.h>
 
 extern void * dna_memcpy (void * s1, const void * s2, size_t n);
-extern void * dna_memset (void *s, int32_t c, size_t n);
-extern int32_t dna_memcmp (const void *s1, const void *s2, size_t n);
+extern void * dna_memset (void * s, int32_t c, size_t n);
+extern int32_t dna_memcmp (const void *s1, const void * s2, size_t n);
 
-extern char * dna_strcpy (char *  s1, const char *  s2);
+extern char * dna_strcpy (char * s1, const char * s2);
 extern char * dna_strcat (char *  dest, const char *  src);
 extern int32_t dna_strcmp (const char *s1, const char *s2);
 extern size_t dna_strlen (const char *s);
