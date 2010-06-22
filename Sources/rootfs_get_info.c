15425a4c269810355a33723833955e7bb2a879f5
Xavier Guérin <xavier.guerin@imag.fr>
2010-06-22 08:43:59 +0200

    Implemented get_info.

--- Sources/rootfs_get_info.c
+++ Sources/rootfs_get_info.c
@@ -21,7 +21,42 @@
 status_t rootfs_get_info (void * ns, void * node,
     void * data, file_info_t * p_info)
 {
+  rootfs_t rootfs = ns;
+  rootfs_inode_t inode = node;
 
-  return DNA_OK;
+  watch (status_t)
+  {
+    ensure (rootfs != NULL, DNA_BAD_ARGUMENT);
+    ensure (inode != NULL, DNA_BAD_ARGUMENT);
+    ensure (p_info != NULL, DNA_BAD_ARGUMENT);
+
+    dna_memset (p_info, 0, sizeof (file_info_t));
+
+    p_info -> volume = rootfs -> vid;
+    p_info -> vnode = inode -> id;
+
+    switch (inode -> class)
+    {
+      case DNA_ROOTFS_FILE :
+        {
+          p_info -> type = DNA_FILE_REGULAR;
+          break;
+        }
+
+      case DNA_ROOTFS_DIRECTORY :
+        {
+          p_info -> type = DNA_FILE_DIRECTORY;
+          break;
+        }
+
+      case DNA_ROOTFS_SYMLINK :
+        {
+          p_info -> type = DNA_FILE_SYMBOLIC_LINK;
+          break;
+        }
+    }
+
+    return DNA_OK;
+  }
 }
 
